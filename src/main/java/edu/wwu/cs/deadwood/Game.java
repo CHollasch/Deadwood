6aa796175abca70c5147ff2f6ab37a6db606d36a
Connor Hollasch <connor@hollasch.net>
2017-11-14 02:53:51 -0800

    Game initialization

--- src/main/java/edu/wwu/cs/deadwood/Game.java
+++ src/main/java/edu/wwu/cs/deadwood/Game.java
@@ -14,7 +14,9 @@ import edu.wwu.cs.deadwood.assets.Role;
 import edu.wwu.cs.deadwood.assets.Room;
 import edu.wwu.cs.deadwood.board.Board;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 
 /**
@@ -25,9 +27,10 @@ public class Game
 {
     private Deadwood deadwood;
     private Collection<Player> players;
-    private Player currentPlayer;
+    private PlayerTurn currentPlayer;
     private Board gameBoard;
-    private int dayNumber;
+
+    private int daysLeft;
 
     private boolean playerUsedMove;
     private boolean playerUsedTurn;
@@ -39,7 +42,6 @@ public class Game
         this.players = new HashSet<>();
         this.currentPlayer = null;
         this.gameBoard = null;
-        this.dayNumber = 1;
 
         this.playerUsedMove = false;
         this.playerUsedTurn = false;
@@ -47,7 +49,46 @@ public class Game
 
     public void initializeGame (final int playerCount)
     {
+        this.daysLeft = 4;
 
+        switch (playerCount) {
+            case 2:
+            case 3:
+                this.daysLeft = 3;
+                break;
+            case 5:
+                this.players.forEach(p -> p.setCreditCount(2));
+                break;
+            case 6:
+                this.players.forEach(p -> p.setCreditCount(4));
+                break;
+            case 7:
+            case 8:
+                this.players.forEach(p -> p.setRank(2));
+                break;
+        }
+
+        final ArrayList<Player> randomPlayerOrder = new ArrayList<>(this.players);
+        Collections.shuffle(randomPlayerOrder);
+
+        PlayerTurn previous = null;
+        for (final Player player : randomPlayerOrder) {
+            final PlayerTurn turn = new PlayerTurn(player);
+
+            if (previous != null) {
+                previous.setNext(turn);
+            }
+
+            if (getCurrentPlayer() == null) {
+                setCurrentPlayer(turn);
+            }
+
+            previous = turn;
+        }
+
+        if (previous != null) {
+            previous.setNext(getCurrentPlayer());
+        }
     }
 
     public void currentPlayerAct ()
@@ -105,9 +146,14 @@ public class Game
         return this.players;
     }
 
-    public Player getCurrentPlayer ()
+    public PlayerTurn getCurrentPlayer ()
     {
         return this.currentPlayer;
+    }
+
+    public void setCurrentPlayer (final PlayerTurn currentPlayer)
+    {
+        this.currentPlayer = currentPlayer;
     }
 
     public Board getGameBoard ()
@@ -115,8 +161,34 @@ public class Game
         return this.gameBoard;
     }
 
-    public int getDayNumber ()
+    public int getDaysLeft ()
     {
-        return this.dayNumber;
+        return this.daysLeft;
+    }
+
+    public static class PlayerTurn
+    {
+        private final Player    player;
+        private PlayerTurn      next;
+
+        public PlayerTurn (final Player player)
+        {
+            this.player = player;
+        }
+
+        public Player getPlayer ()
+        {
+            return this.player;
+        }
+
+        public PlayerTurn getNext ()
+        {
+            return this.next;
+        }
+
+        public void setNext (final PlayerTurn next)
+        {
+            this.next = next;
+        }
     }
 }
